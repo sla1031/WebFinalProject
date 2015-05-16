4f639b03f2ad3d37fdcca1eb55682c9973abc16b
sla1031 <sashley2@radford.edu>
2015-05-16 12:21:42 -0400

    updated db info

--- php/utils.php
+++ php/utils.php
@@ -1,13 +1,4 @@
 <?php
-require_once("my-db-login-info.php");
 
-function openConnection(){
-	$conn = mysqli_connect("localhost", $dbuser, $dbpass, $db);
-	echo  ($conn ? "" : "Connection NOT established.<br />\n");
-
-	$sqlStmt = mysqli_stmt_init($conn);
-	
-	
-}
 
 ?>
